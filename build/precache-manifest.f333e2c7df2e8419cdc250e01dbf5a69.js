e2c7e608806bd666ff198aa15620daf7eab8977e
Lucas Henrique Paes de Carvalho <lukas.paes18@gmail.com>
2021-08-12 03:46:36 -0300

    teste

---
+++ build/precache-manifest.f333e2c7df2e8419cdc250e01dbf5a69.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d497cbbc71bbaa20443d9064542cbec6",
+    "url": "/curriculum/index.html"
+  },
+  {
+    "revision": "54dffddbb289bf7c0b8d",
+    "url": "/curriculum/static/css/2.8b275f32.chunk.css"
+  },
+  {
+    "revision": "54dffddbb289bf7c0b8d",
+    "url": "/curriculum/static/js/2.fec92795.chunk.js"
+  },
+  {
+    "revision": "d8e6b9b6d6dc198439d65c8fa1b66951",
+    "url": "/curriculum/static/js/2.fec92795.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc6a9081acd38666430",
+    "url": "/curriculum/static/js/main.589056cb.chunk.js"
+  },
+  {
+    "revision": "b1837890ae22730d8ac7",
+    "url": "/curriculum/static/js/runtime-main.8cbb7332.js"
+  },
+  {
+    "revision": "c762d850e2e8d0e29047608715196736",
+    "url": "/curriculum/static/media/roboto-all-400-italic.c762d850.woff"
+  },
+  {
+    "revision": "a91ad097d24828af724d4fee36a063ed",
+    "url": "/curriculum/static/media/roboto-all-400-normal.a91ad097.woff"
+  },
+  {
+    "revision": "8266321ab43353bcd147ad67600d165c",
+    "url": "/curriculum/static/media/roboto-cyrillic-400-italic.8266321a.woff2"
+  },
+  {
+    "revision": "8bb64952764a884d67019b3486296ab9",
+    "url": "/curriculum/static/media/roboto-cyrillic-400-normal.8bb64952.woff2"
+  },
+  {
+    "revision": "b3e580d221a4722c959155878ab94210",
+    "url": "/curriculum/static/media/roboto-cyrillic-ext-400-italic.b3e580d2.woff2"
+  },
+  {
+    "revision": "4743c758a952f2bd4a35d4e42afc002b",
+    "url": "/curriculum/static/media/roboto-cyrillic-ext-400-normal.4743c758.woff2"
+  },
+  {
+    "revision": "469a78405c3ae073ba769321fa0584f3",
+    "url": "/curriculum/static/media/roboto-greek-400-italic.469a7840.woff2"
+  },
+  {
+    "revision": "c1e9793c84cb26c44ef2a2cf8b6f49ce",
+    "url": "/curriculum/static/media/roboto-greek-400-normal.c1e9793c.woff2"
+  },
+  {
+    "revision": "801b64f75ae26dab9c36f00629ebdad0",
+    "url": "/curriculum/static/media/roboto-greek-ext-400-italic.801b64f7.woff2"
+  },
+  {
+    "revision": "182ee6a4872ca8fa78048951b1561a5c",
+    "url": "/curriculum/static/media/roboto-greek-ext-400-normal.182ee6a4.woff2"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/curriculum/static/media/roboto-latin-400-italic.51521a2a.woff2"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/curriculum/static/media/roboto-latin-400-normal.479970ff.woff2"
+  },
+  {
+    "revision": "dc3871f486e189164db4b98968330bc4",
+    "url": "/curriculum/static/media/roboto-latin-ext-400-italic.dc3871f4.woff2"
+  },
+  {
+    "revision": "455200cb007fe1212c668721d827c691",
+    "url": "/curriculum/static/media/roboto-latin-ext-400-normal.455200cb.woff2"
+  },
+  {
+    "revision": "9780bde87bceec579eaf16bddad47615",
+    "url": "/curriculum/static/media/roboto-vietnamese-400-italic.9780bde8.woff2"
+  },
+  {
+    "revision": "a8be5b46d06bb541b0968196ee5e6bb8",
+    "url": "/curriculum/static/media/roboto-vietnamese-400-normal.a8be5b46.woff2"
+  }
+]);
